13a5a1abb752064509aa9a6a3ef76f1b5681056b
Pavel Bal <pbal00@mail.ru>
2022-10-01 18:32:17 +0300

    feat: add transition for blackout

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,6 +1,26 @@
 function closeSideMenu() {
-  document.getElementById('menu-toggle').checked = false;
+  const checkbox = document.getElementById('menu-toggle');
+  checkbox.checked = false;
+  checkbox.dispatchEvent(new Event('change'));
 }
+function disableScrolling() {
+  const bodyElem = document.querySelector('body');
+  bodyElem.style.overflow = 'hidden';
+}
+function enableScrolling() {
+  const bodyElem = document.querySelector('body');
+  bodyElem.style.overflow = 'auto';
+}
+function scrollingController(e) {
+  if (e.target.checked) disableScrolling(); else enableScrolling();
+}
+
 document
   .querySelectorAll('.menubox li')
   .forEach((item) => item.addEventListener('click', closeSideMenu));
+document
+  .querySelectorAll('.blackout')
+  .forEach((item) => item.addEventListener('click', closeSideMenu));
+document
+  .querySelector('#menu-toggle')
+  .addEventListener('change', scrollingController);
