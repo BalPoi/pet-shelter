3284e05ea8022e8e2b4df5811aa7df58b73201ff
Pavel Bal <pbal00@mail.ru>
2022-10-02 16:43:57 +0300

    feat: implement infinity pets carousel

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -25,22 +25,61 @@ document
   .querySelector('#menu-toggle')
   .addEventListener('change', scrollingController);
 
-async function petCarouselInit() {
-  const carousel = document.querySelector('.pet-slider > .cards_wrapper');
-  const prevBtn = document.querySelector('.pet-slider > .arrow.prev');
-  const nextBtn = document.querySelector('.pet-slider > .arrow.next');
-  const sliderLength = getComputedStyle(carousel).getPropertyValue('--slider-length');
-  const petsArray = await fetch('../../assets/pets.json').then((resp) => resp.json());
+function petCarouselNext() {
+  let start = petCarouselRender.startPetIndex;
+  let length = petCarouselRender.sliderLength;
+  let pets = petCarouselRender.pets;
+  if (start + length > pets.length - 1) {
+    petCarouselRender.startPetIndex = start + length - pets.length;
+  } else {
+    petCarouselRender.startPetIndex += length;
+  }
+  petCarouselRender();
+}
+function petCarouselPrev() {
+  let start = petCarouselRender.startPetIndex;
+  let length = petCarouselRender.sliderLength;
+  let pets = petCarouselRender.pets;
+  if (start - length < 0) {
+    petCarouselRender.startPetIndex = start - length + pets.length;
+  } else {
+    petCarouselRender.startPetIndex -= length;
+  }
+  petCarouselRender();
+}
+function petCarouselRender() {
+  let start = petCarouselRender.startPetIndex;
+  let length = petCarouselRender.sliderLength;
+  let pets = petCarouselRender.pets;
+  petCarouselRender.carousel.innerHTML = '';
+  for (let i = 0; i < length; i++) {
+    const index = i + start <= pets.length-1
+      ? i + start
+      : (pets.length - i - start) * -1;
 
-  for (let i = 0; i < sliderLength; i++) {
-    carousel.innerHTML += `
+    petCarouselRender.carousel.innerHTML += `
       <div class="pet_card">
-        <img src="${petsArray[i].img}" alt="${petsArray[i].name}" />
-        <div class="pet_name">${petsArray[i].name}</div>
+        <img src="${pets[index].img}" alt="${pets[index].name}" />
+        <div class="pet_name">${pets[index].name}</div>
         <button>Learn more</button>
       </div>
     `;
   }
 }
+(async function petCarouselInit() {
+  const carousel = document.querySelector('.pet-slider > .cards_wrapper');
+  const prevBtn = document.querySelector('.pet-slider > .arrow.prev');
+  const nextBtn = document.querySelector('.pet-slider > .arrow.next');
+  const sliderLength = Number.parseInt(getComputedStyle(carousel).getPropertyValue('--slider-length'));
+  const petsArray = await fetch('../../assets/pets.json').then((resp) => resp.json());
 
-petCarouselInit();
+  petCarouselRender.pets = petsArray;
+  petCarouselRender.startPetIndex = 0;
+  petCarouselRender.sliderLength = sliderLength;
+  petCarouselRender.carousel = carousel;
+
+  petCarouselRender();
+
+  prevBtn.addEventListener('click', petCarouselPrev);
+  nextBtn.addEventListener('click', petCarouselNext);
+})();
